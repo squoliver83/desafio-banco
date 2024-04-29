d6ab1b571f1fb40c699595df3e29853000eef085
Samuel Quaresma <squoliver@gmail.com>
2024-04-29 16:52:25 -0300

    Encapsulamento

--- src/entities/Banco.java
+++ src/entities/Banco.java
@@ -2,6 +2,15 @@ package entities;
 
 public class Banco {
 
-    public String nome;
+    private String nome;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
 
 }
--- src/entities/Cliente.java
+++ src/entities/Cliente.java
@@ -2,5 +2,13 @@ package entities;
 
 public class Cliente {
 
-    public String nome;
+    private String nome;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
 }
